1e3119ded4727dc2d28affee000034105a64e67b
John Regner <john@johnregner.com>
2014-05-08 21:49:16 -0500

    Add GenerateResume.sh script, and files required to create my pdf resume.
    tex_*.tex are required to take the mmd2tex output and make it pdf generate-able.
    When GenerateResume.sh is run it creates the JohnRegnerResume.html using the standard
    markdown generator. And the resume.pdf is created by using a mmd2tex then adding in header
    and footer stuff, running pdflatex on that then cleaning up the latex mess.

--- resume/GenerateResume.sh
+++ resume/GenerateResume.sh
@@ -1,1 +1,31 @@
+#!/usr/bin/env bash
+
+#generate html resume page in the correct format for display on web
 markdown resume.txt > JohnRegnerResume.html
+
+#Create resume.tex, to be latex processed into a pdf
+mmd2tex resume.txt
+
+#remove the first 4 lines of the generated tex, you do not need them.
+sed '1,4d' resume.tex > resume1.tex
+
+#remove the work history, header, it is obvious.
+sed '10,11d' resume1.tex > resume.tex
+
+#clean up resume1.tex
+rm resume1.tex
+
+#put the tex files together, substitute the part for the NewPart
+cat tex_header.tex resume.tex tex_footer.tex | sed 's_\part_\NewPart_g' > resume_temp.tex
+
+sed -e '116r tex_pagebreak.tex' resume_temp.tex > resume.tex
+
+pdflatex resume.tex
+# run 2x just to make sure you get the labels right
+pdflatex resume.tex
+
+#clean up latex artifacts
+rm resume_temp.tex
+rm resume.log
+rm resume.tex
+rm resume.aux
